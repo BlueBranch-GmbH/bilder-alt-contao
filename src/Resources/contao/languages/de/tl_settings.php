5aacb70065db52f265b62dbfcf14b9bc93f9297f
LBeckX <lb@bluebranch.de>
2025-10-07 12:31:31 +0200

    Fix typo;

--- src/Resources/contao/languages/de/tl_settings.php
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -2,7 +2,7 @@
 
 $GLOBALS['TL_LANG']['tl_settings']['bilder_alt_legend'] = 'SEO Alt Text Generator Einstellungen';
 $GLOBALS['TL_LANG']['tl_settings']['bilderAltApiKey'] = ['API-Key', 'API-Key eingeben. Kostenlos hier holen https://app.bilder-alt.de/'];
-$GLOBALS['TL_LANG']['tl_settings']['bilderAltAutoGenerate'] = ['Automatsch Alt-Tags generieren', 'Wenn aktiv, werden Alt-Tags automatisch nach dem Upload generiert.'];
+$GLOBALS['TL_LANG']['tl_settings']['bilderAltAutoGenerate'] = ['Automatisch Alt-Tags generieren', 'Wenn aktiv, werden Alt-Tags automatisch nach dem Upload generiert.'];
 $GLOBALS['TL_LANG']['tl_settings']['bilderAltAddExistingAltTag'] = ['Bestehende Alt-Texte mitsenden', 'Wenn aktiv, werden bestehende Alt-Texte als Keywords mitgeschickt um den Context zu behalten.'];
 $GLOBALS['TL_LANG']['tl_settings']['bilderAltKeywords'] = ['Keywords', 'Kommagetrennte Keyword-Liste angeben, welche für SEO berücksichtigt werden.'];
 $GLOBALS['TL_LANG']['tl_settings']['bilderAltCredits'] = ['Verfügbare Credits', 'Credits aufladen: https://app.bilder-alt.de/'];
